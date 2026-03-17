bf80e5de4f9acdcdcd1a457b21d2e0bb2d3903e8
Levi22 <levihdbr@gmail.com>
2026-03-17 09:52:23 -0300

    Atualizacao

--- js.js
+++ js.js
@@ -1,9 +1,10 @@
-const numero = Math.floor(Math.random() * 10) ;
-
+const numero = Math.floor(Math.random() * 100) ;
+let tentativas = 10;
 function botao(){
     var valor = document.getElementById("valor").value
     console.log(valor)
     console.log(numero)
+    tentativas = tentativas - 1
     document.getElementById("Numero").textContent = "Número que você colocou: " + valor
     if(valor == numero){
         document.getElementById("resultado").textContent = "Parabéns! Você acertou!"
@@ -13,4 +14,11 @@ function botao(){
     else if(valor < numero){
         document.getElementById("resultado").textContent = "O número é maior do que o que você colocou."
     }
+
+    document.getElementById("tentativas").textContent   = "Número de tentativas: " + tentativas
+    if(tentativas == 0){
+        document.getElementById("resultado").textContent = "Suas tentativas acabaram! O número era: " + numero
+        document.getElementById("valor").disabled = true
+        document.getElementById("butao").disabled = true
+    }    
 }
